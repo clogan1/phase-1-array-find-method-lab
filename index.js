59257a040d5454952e3f79345a5cf5b423dc6b7b
Claire <clairelogan16@gmail.com>
2021-07-05 15:47:12 -0700

    commit tree 91d504035eb413146131641c97ba899398b707bd

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 
-function superbowlWin(record){
-    const winList = record.find(obj => obj.result === 'W'); //return an element if condition met
-    //if (record.find(obj => obj.result === 'W') ) 
-    if (winList) { //if winList is "true" meaning it has an element, then return the year; if its "false" meaning it's empty, then undefined
-        return winList.year
-    } else {return undefined}
+// function superbowlWin(record){
+//     const winList = record.find(obj => obj.result === 'W'); //return an element if condition met
+//     if (winList) { //if winList is "true" meaning it has an element, then return the year; if its "false" meaning it's empty, then undefined
+//         return winList.year
+//     } else {return undefined}
 
-    // const winList = record.find(obj => obj.result === 'W');
-    //return winList.year; //passes 1st part
+//     // const winList = record.find(obj => obj.result === 'W');
+//     //return winList.year; //passes 1st part
 
 
-    //return winList; ==> passes 2nd part (undefined)
+//     //return winList; ==> passes 2nd part (undefined)
+// }
+
+
+
+
+
+function superbowlWin(arr) {
+    const winYear = arr.find(season => {
+        return season.result === 'W';
+    })
+    if (winYear) {
+        return winYear.year;
+    } else {
+        return undefined}
+    ;
 }
+
+
+
+
+
+
+
